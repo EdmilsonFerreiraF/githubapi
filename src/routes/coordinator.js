20668343221318cfc8de5459d30d0fa315945f55
EdmilsonFerreiraF <Edmilsonf.o3@gmail.com>
2021-07-10 23:23:44 -0300

    🚀 :rocket: [Add] Search user details
    Details about user from search results

--- src/routes/coordinator.js
+++ src/routes/coordinator.js
@@ -6,6 +6,10 @@ export const goToAuth = (history) => {
     history.push('http://localhost:3003/user/login/auth')
 }
 
-export const goToSearch = (history, query) => {
-    history.push(`/search?query=${query}`)
+export const goToSearch = (history, username) => {
+    history.push(`/search?username=${username}`)
+}
+  
+export const goToSearchDetails = (history, username) => {
+    history.push(`/users/${username}`)
 }
--- src/screens/SearchList/SearchList.js
+++ src/screens/SearchList/SearchList.js
@@ -16,22 +16,23 @@ const SearchList = () => {
 
     let query = useQuery();
 
-    const result = useRequestData(`${GITHUB_API_URL}/search/users?q=${query.get("query")}`, [])
+    const result = useRequestData(`${GITHUB_API_URL}/search/users?q=${query.get("username")}`, [])
 
+    console.log(result)
     return (
         <div className="my-5 gy-3 row">
             <h1 className="display-4 text-center mb-5 title">Github API
                 Search
             </h1>
 
-            {!query.get("query") && <p className="text-center lead">Pesquise por usuário, repositório ou repositórios mais visitados no campo de pesquisa</p>}
+            {!query.get("username") && <p className="text-center lead">Pesquise por usuário, repositório ou repositórios mais visitados no campo de pesquisa</p>}
             {result && !result.items && <p className="text-center lead">Nenhum resultado encontrado.</p>}
 
             <ul className="row my-5 gap-4 justify-content-center">
                 {result && result.items && result.items.map(result => (
                     <SearchItem 
                         key={result.id}
-                        query={query.get("query")}
+                        username={query.get("username")}
                         avatar_url={result.avatar_url}
                     />
                 ))}
