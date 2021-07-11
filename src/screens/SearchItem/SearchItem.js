8a03cf371c4b452f91aa9dfaf28c2ea93a2ebba8
EdmilsonFerreiraF <Edmilsonf.o3@gmail.com>
2021-07-10 23:50:42 -0300

    🚀 :rocket: [Add] Search user repos
    User repos page by search

--- src/screens/SearchItem/SearchItem.js
+++ src/screens/SearchItem/SearchItem.js
@@ -1,19 +1,19 @@
 import React from "react"
 import { useHistory } from "react-router-dom";
 
-import { goToSearchDetails } from "../../routes/coordinator";
+import { goToSearchDetails, goToSearchItemRepositories } from "../../routes/coordinator";
 
 const SearchItem = (props) => {
     const history = useHistory();
   
     return (
-      <div className="card result-item" onClick={() => goToSearchDetails(history, props.username)}>
-        <img src={props.avatar_url} className="card-img-top" alt="User avatar" />
-        <div className="card-body">
+      <div className="card result-item">
+        <img src={props.avatar_url} className="card-img-top" alt="User avatar" onClick={() => goToSearchDetails(history, props.username)}/>
+        <div className="card-body"  onClick={() => goToSearchDetails(history, props.username)}>
           <h5 className="card-title">{props.username}</h5>
         </div>
         <div className="card-body row gap-3">
-            <button type="button" className="btn btn-warning col-md result-item__btn">Repositórios</button>
+            <button type="button" className="btn btn-warning col-md result-item__btn" onClick={() => goToSearchItemRepositories(history, props.username)}>Repositórios</button>
             <button type="button" className="btn btn-warning col-md result-item__btn">Repos. mais visitados</button>
         </div>
       </div>
